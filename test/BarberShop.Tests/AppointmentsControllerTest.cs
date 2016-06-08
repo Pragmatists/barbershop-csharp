ebae8cf0753102550e69fbdbe20f56026d7cb306
Krzysztof Jelski <krzysztof.jelski@pragmatists.pl>
2016-06-08 19:15:37 +0200

    Finally found which library to update.

---
+++ test/BarberShop.Tests/AppointmentsControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Xunit;
+
+namespace BarberShop.Tests
+{
+  public class AppointmentsControllerTest
+  {
+    private HttpClient client;
+    private TestServer server;
+
+    [Fact]
+    public async Task Lists_Appointments()
+    {
+            
+      server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+      client = server.CreateClient();
+
+      var response = await client.GetAsync("/api/appointments");
+      response.EnsureSuccessStatusCode();
+
+      var responseString = await response.Content.ReadAsStringAsync();
+
+      Assert.Equal("", responseString);
+    }
+
+  }
+}
